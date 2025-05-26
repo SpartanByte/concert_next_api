224090c9b3cb86bec0688a4f85d6ad7033465950
Brian Wardwell <briwar10@outlook.com>
2025-05-26 02:33:29 -0500

    update next config to use typescript

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
-
+/* Update for Typescript*/
+import type { NextConfig } from 'next'
+ 
 const nextConfig: NextConfig = {
   /* config options here */
-};
-
-export default nextConfig;
+}
+ 
+export default nextConfig
