6e910f679e921754c8acf96df4dba80f470392af
Brian Wardwell <briwar10@outlook.com>
2025-06-15 21:50:32 -0500

    start adding test components

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+import Date from '../components/date';
+
+<li className={utilStyles.listItem} key={id}>
+  <Link href={`/posts/${id}`}>{title}</Link>
+  <br />
+  <small className={utilStyles.lightText}>
+    <Date dateString={date} />
+  </small>
+</li>
